a2f88f278cfe3c53e8210e8542d62d920481165d
Coltan Cristian <cristicoltan0202@yahoo.com>
2025-11-23 12:38:00 +0200

    [frontend] svgs and images

---
+++ resources/assets/svgs/index.js
@@ -0,0 +1,14 @@
+import InstagramIcon from '../svgs/instagram-1-svgrepo-com.svg'
+import VisaIcon from '../svgs/visa-svgrepo-com.svg'
+import PayPalIcon from '../svgs/paypal-svgrepo-com.svg'
+import MastercardIcon from '../svgs/mastercard-full-svgrepo-com.svg'
+import TikTokIcon from '../svgs/tiktok-svgrepo-com.svg'
+import FacebookIcon from '../svgs/facebook-svgrepo-com.svg'
+export const icons = {
+    instagram: InstagramIcon,
+    visa: VisaIcon,
+    paypal: PayPalIcon,
+    mastercard: MastercardIcon,
+    tiktok: TikTokIcon,
+    facebook: FacebookIcon,
+}
